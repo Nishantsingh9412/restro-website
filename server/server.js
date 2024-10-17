32feff83a6e5e08646a5ec99c120518c0cfff363
Nijamuddin Ansari <76437265+nizxcoder@users.noreply.github.com>
2024-10-17 17:45:19 +0530

    added new frontend files to the static files

--- server/server.js
+++ server/server.js
@@ -58,11 +58,11 @@ const __dirname = path.resolve(); // Set the __dirname to current directory
 
 // Serve static files in production (e.g., frontend build files)
 if (process.env.NODE_ENV !== "production") {
-  app.use(express.static("./frontend/build"));
+  app.use(express.static("./frontend/dist"));
 
   // Serve React app for any unknown routes in production
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./frontend", "dist", "index.html"));
   });
 } else {
   // Development mode base route
